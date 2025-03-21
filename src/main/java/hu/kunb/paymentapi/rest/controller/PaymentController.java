3ecdca4f0cdf969cc0414d9aa479b53ff591147c
DWP8053 <balazs.kun.ext@dwpbank.de>
2025-03-21 05:06:17 +0100

    openapi modified

--- src/main/java/hu/kunb/paymentapi/rest/controller/PaymentController.java
+++ src/main/java/hu/kunb/paymentapi/rest/controller/PaymentController.java
@@ -1,8 +1,10 @@
 package hu.kunb.paymentapi.rest.controller;
 
 import jakarta.validation.Valid;
+import java.util.UUID;
 import org.openapitools.api.PaymentsApi;
 import org.openapitools.model.Payment;
+import org.openapitools.model.PaymentResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,8 +17,8 @@ public class PaymentController implements PaymentsApi {
 
   @Override
   @PostMapping
-  public ResponseEntity<Payment> createPayment(@Valid @RequestBody Payment payment) {
-    return ResponseEntity.status(201).body(payment);
+  public ResponseEntity<org.openapitools.model.PaymentResponse> createPayment(@Valid @RequestBody Payment payment) {
+    return ResponseEntity.status(201).body(new PaymentResponse().transactionId(UUID.randomUUID()));
   }
 
 }
